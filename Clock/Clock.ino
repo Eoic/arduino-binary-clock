f2d57d7548c155ddd854db8bd01326135aee43fd
Karolis <karolis.strazdas@ktu.edu>
2018-08-01 22:32:56 +0300

    Update positions of LED outputs

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -28,11 +28,11 @@ void setup()
 {
 
 	/*
-     PINS     [2;11]
-     Hours:   [2;5]
-     Minutes: [6;11]
-     Buttons: [12;13]
-  */
+		PINS     [2;11]
+		Hours:   [2;5]
+		Minutes: [6;11]
+		Buttons: [12;13]
+  	*/
 
 	for (int i = 2; i <= 11; i++)
 		pinMode(i, OUTPUT);
@@ -49,33 +49,37 @@ void loop()
 	// Toggle time set mode.
 	if (digitalRead(hoursBtnPin) == HIGH)
 	{
-    // Set button as pressed down and save timestamp.
-    if(!hoursButtonDown){
-  	  hoursButtonDown = true;
-  	  buttonTimer = millis();
-    } 
-    
-    if((millis() - buttonTimer) >= longPressTime && !longPressIssued) {
-      longPressActive = !longPressActive;
-      longPressIssued = true;
+		// Set button as pressed down and save timestamp.
+		if (!hoursButtonDown)
+		{
+			hoursButtonDown = true;
+			buttonTimer = millis();
+		}
 
-      if(!longPressActive)
-        hourOffset = (hourOffset - 1) % 12;
-    } 
-    
-    if(longPressActive && !hourUpdated && !longPressIssued){
-        hourOffset = (hourOffset + 1) % 12;
-        hourUpdated = true;
-    } 
+		if ((millis() - buttonTimer) >= longPressTime && !longPressIssued)
+		{
+			longPressActive = !longPressActive;
+			longPressIssued = true;
 
-    delay(50);
+			if (!longPressActive)
+				hourOffset = (hourOffset - 1) % 12;
+		}
+
+		if (longPressActive && !hourUpdated && !longPressIssued)
+		{
+			hourOffset = (hourOffset + 1) % 12;
+			hourUpdated = true;
+		}
+
+		delay(50);
 	}
 
-  if(digitalRead(hoursBtnPin) == LOW && hoursButtonDown){
-    hoursButtonDown = false;
-    hourUpdated = false;
-    longPressIssued = false;
-  }
+	if (digitalRead(hoursBtnPin) == LOW && hoursButtonDown)
+	{
+		hoursButtonDown = false;
+		hourUpdated = false;
+		longPressIssued = false;
+	}
 
 	// Set minutes.
 	if (digitalRead(minutesBtnPin) == HIGH && longPressActive && !minuteUpdated)
@@ -83,27 +87,27 @@ void loop()
 		minuteOffset++;
 		minuteUpdated = true;
 
-    delay(50);
+		delay(50);
 	}
 
-	if (digitalRead(minutesBtnPin) == LOW){
+	if (digitalRead(minutesBtnPin) == LOW)
+	{
 		minuteUpdated = false;
-
-    delay(50);
+		delay(50);
 	}
 
-  // Total time in ms.
-  time = millis() + (hourOffset * 3600000) + (minuteOffset * 60000);
-  
+	// Total time in ms.
+	time = millis() + (hourOffset * 3600000) + (minuteOffset * 60000);
+
 	// Calculate time from program start and add given offset.
-  minutes = (time / 60000) % 60;
+	minutes = (time / 60000) % 60;
 	hours = (time / 3600000) % 12;
 
 	// Set output pins for hours.
 	for (int i = 0; i < 4; i++)
-		digitalWrite(5 - i, (bitRead(hours, i) ? HIGH : LOW));
+		digitalWrite(i + 2, (bitRead(hours, i) ? HIGH : LOW));
 
 	// Set output pins for minutes.
 	for (int i = 0; i < 6; i++)
-		digitalWrite(11 - i, (bitRead(minutes, i) ? HIGH : LOW));
+		digitalWrite(i + 6, (bitRead(minutes, i) ? HIGH : LOW));
 }
